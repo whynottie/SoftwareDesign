da0c34ce3f64688e1b097f0fa1574869c74cbc0d
ganesh343 <50006173+ganesh343@users.noreply.github.com>
2022-03-13 13:39:25 -0500

    Update login.js

--- login.js
+++ login.js
@@ -38,7 +38,7 @@ window.onload = function () {
 		}
 		else{
 			sessionStorage.setItem("FQSESSION_username",enteredUsername);
-			window.location = "mainmenu.html";
+			window.location = "account.html";
 		}
 	});
 };
