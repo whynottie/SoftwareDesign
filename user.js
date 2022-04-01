c5c3fdf6e6a39d44dbdb4be859e8ec73951cf8d6
ganesh343 <50006173+ganesh343@users.noreply.github.com>
2022-04-01 02:03:58 -0500

    Add files via upload

--- user.js
+++ user.js
@@ -1,15 +1,9 @@
-const mongoose = require(mongoose)
+const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-    username: {
-        type : String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    profile : [profileSchema]
-})
+const userSchema = new mongoose.Schema ({
+    username: {type: String, required: true, trim: true, minlength: 8},
+    password:{type:String,required:true, trim: true, minlength: 8},
+});
 
-module.exports = mongoose.model('User', userSchema)
+const Users = mongoose.model("User", userSchema);
+module.exports = Users;
